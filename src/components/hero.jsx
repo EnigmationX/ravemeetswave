7a4ea5a830ed7b3c9efc7f2150c73d00355f9308
uth.gbolahan@gmail.com <uth.gbolahan@gmail.com>
2025-11-10 15:15:57 +0100

    made path changes for hero-bg & pv to public

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,6 +1,5 @@
 import React,{useRef} from 'react'
-import bgurl from '../assets/images/hero-bg.png'
-import '../font.css'
+ import '../font.css'
 import { useGSAP } from '@gsap/react'
 import { SplitText } from 'gsap/all'
 import gsap from 'gsap'
@@ -33,7 +32,7 @@ useGSAP(()=>{
    return (
     <section className='overflow-hidden'>
     <div className='hero-container w-screen  bg-amber-200 h-dvh relative'>
-         <img src={bgurl} alt="RaveMeetWave Hero Background" className="w-full  sm:scale-100  
+         <img src="./hero=bg.PNG" alt="RaveMeetWave Hero Background" className="w-full  sm:scale-100  
          scale-150 z-10 transistion-transform duration-300 absolute -bottom-0 object-cover"/>
          <span  className ="bubble hero-title absolute text-center  top-1/4 
          translate-y-1/4 right-1/2 text-5xl text-black scale-100 sm:scale-200   translate-x-1/2"
--- src/components/v-section.jsx
+++ src/components/v-section.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import vdurl from '../assets/videos/thriller.mp4'
-import playtext from "../assets/images/pv.png"
  import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
 export default function vsection() {
@@ -28,7 +27,7 @@ export default function vsection() {
                 className="absolute inset-0 w-full h-full"
                 />
                 <div className='absolute top-1/2 left-1/2 -translate-x-1/2  animate-spin [animation-duration:25s] -translate-y-1/2'>
-                    <img src={playtext} className='h-15 md:h-25'/>
+                    <img src="./pv.png" className='h-15 md:h-25'/>
                 </div>
                  <div className='absolute top-1/2 left-1/2 -translate-x-1/2 border rounded-full  p-1 md:p-3 backdrop-blur-lg border-b border-white/20 -translate-y-1/2'>
                     <img src='./play.svg' className='h-5 md:h-10'/>
